d747673557d5986119b3e19fe6f711fa93671b1d
aryan-Raj404 <aryanraj120107@gmail.com>
2026-02-14 16:09:27 +0530

    correcting-index.js

--- backend/index.js
+++ backend/index.js
@@ -39,10 +39,9 @@ app.use("/api/notification",notificationRouter);
 app.use('/api/messages', messageRoutes);
 
 if(process.env.NODE_ENV === "production"){
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-	app.get("*",(req,res)=>{
-		res.sendFile(path.resolve(__dirname,"frontend", "dist", "index.html"));
-	})
+	app.get('/(.*)', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 }
 
 const PORT = process.env.PORT || 3000
