f982f08719dc453da549241946ac7c65a89b5bf4
aryan-Raj404 <aryanraj120107@gmail.com>
2026-02-14 16:20:03 +0530

    last-commit

--- backend/index.js
+++ backend/index.js
@@ -45,7 +45,7 @@ if (process.env.NODE_ENV === "production") {
     // 2. Use a Regular Expression /.*/ instead of the string "*"
     app.get(/.*/, (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
+    })
 }
 
 const PORT = process.env.PORT || 3000
