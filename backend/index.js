05a4193592976373bf07d3c235cf97450a6695b2
aryan-Raj404 <aryanraj120107@gmail.com>
2026-02-14 16:15:46 +0530

    done-change

--- backend/index.js
+++ backend/index.js
@@ -38,10 +38,11 @@ app.use("/api/post",postRouter);
 app.use("/api/notification",notificationRouter);
 app.use('/api/messages', messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
-	app.get('/(.*)', (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend", "dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 }
 
 const PORT = process.env.PORT || 3000
